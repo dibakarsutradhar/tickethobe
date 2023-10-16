d4b45108f23edd63fc3ada565824e62ac7d6fa41
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2023-10-17 00:04:26 +0600

    scrollSepolia hardhat config

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -1,6 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+	solidity: '0.8.19',
+	networks: {
+		scrollSepolia: {
+			url: 'https://sepolia-rpc.scroll.io/' || '',
+			accounts:
+				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
+	},
+	etherscan: {
+		apiKey: {
+			scrollSepolia: '',
+		},
+		customChains: [
+			{
+				network: 'scrollSepolia',
+				chainId: 534351,
+				urls: {
+					apiURL: `https://sepolia-blockscout.scroll.io/api`,
+					browserURL: 'https://sepolia-blockscout.scroll.io/',
+				},
+			},
+		],
+	},
 };
